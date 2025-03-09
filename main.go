d28fdb8ced4840844f7e76a690f1d5b2fe4551d6
Santiago Gonzalez <punigonzalez07@gmail.com>
2025-03-09 08:06:07 -0300

    Se agregan ejemplos de variables, funciones y arrays

--- main.go
+++ main.go
@@ -2,19 +2,30 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-
-	"github.com/gin-gonic/gin"
 )
+
+func sumar(a int, b int) int {
+	return a + b
+}
 
 func main() {
 
 	fmt.Println("¡Hola, Mundo!")
-	router := gin.Default()
 
-	router.GET("/saludo", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"mensaje": "Hola, bienvenido a tu app con Gin!"})
-	})
+	resultado := sumar(2, 4)
+	fmt.Println("Resultado:", resultado)
 
-	router.Run(":8080")
+	edad := 13
+
+	if edad >= 18 {
+		fmt.Println("Eres mayor de edad")
+	} else {
+		fmt.Println("Eres menor de edad :/")
+	}
+
+	frutas := []string{"Manzana", "Banana", "Naranja"}
+	fmt.Println("Array de frutas:", frutas)
+	frutas = append(frutas, "Pera")
+	fmt.Println("Slice de frutas:", frutas)
+
 }
